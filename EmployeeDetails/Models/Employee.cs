c8a3188526c358235fdd7a0562e3c5e0fd935150
Pradeep gauti <prada0328@gmail.com>
2022-05-30 15:07:30 +0530

    Add project files.

---
+++ EmployeeDetails/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeDetails.Models
+{
+    public class Employee
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("DateOfBirth")]
+        public string DateOfBirth { get; set; }
+
+        [JsonProperty("PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("EmailId")]
+        public string EmailId { get; set; }
+
+        [JsonProperty("EmployeeId")]
+        public string EmployeeId { get; set; }
+
+        [JsonProperty("id")]
+        public string EmployeeGuid { get; set; }
+    }
+    public class Employees
+    {
+        public Employees()
+        {
+            LstEmployee = new List<Employee>();
+        }
+        public List<Employee> LstEmployee { get; set; }
+    }
+}
